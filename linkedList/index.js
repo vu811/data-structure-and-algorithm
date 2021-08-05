13686feae595c3374625f96179d2ddf2606c05f7
vunh811@gmail.com <vunh811@gmail.com>
2021-08-05 22:17:28 +0700

    sort linked list

--- linkedList/index.js
+++ linkedList/index.js
@@ -22,11 +22,36 @@ class LinkedList {
       currentNode.next = newNode
     }
   }
+
+  sort() {
+    if (this.head === null) return
+
+    let currentNode = this.head
+    let compareCurrentNode = null
+    let temp = null
+
+    while (currentNode.next !== null) {
+      compareCurrentNode = currentNode.next
+      while (compareCurrentNode !== null) {
+        if (currentNode.data > compareCurrentNode.data) {
+          temp = currentNode.data
+          currentNode.data = compareCurrentNode.data
+          compareCurrentNode.data = temp
+        }
+        compareCurrentNode = compareCurrentNode.next
+      }
+      currentNode = currentNode.next
+    }
+  }
 }
 
 let linkedList = new LinkedList()
-linkedList.insert(1)
+linkedList.insert(11)
 linkedList.insert(5)
-linkedList.insert(7)
+linkedList.insert(2)
+linkedList.insert(9)
+linkedList.insert(1)
+
+linkedList.sort()
 
 console.log(linkedList)
